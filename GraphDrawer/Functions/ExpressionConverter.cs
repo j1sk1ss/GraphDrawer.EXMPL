a282618a1bc55a3687615dfb6a01f1cc6e4e2972
j1sk1ss <fotikolay222@gmail.com>
2022-12-11 16:00:16 +0500

    ver .1

---
+++ GraphDrawer/Functions/ExpressionConverter.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace GraphDrawer.Functions {
+    public static class ExpressionConverter {
+        private static readonly string[] Operators = { "-", "+", "/", "*","^","sin","cos", "tg", "ctg", "="};
+        private static readonly Func<double, double, double>[] Operations = {
+            (a1, a2) => a1 - a2,
+            (a1, a2) => a1 + a2,
+            (a1, a2) => a1 / a2,
+            (a1, a2) => a1 * a2,
+            Math.Pow,
+            (a1, a2) => a1 * Math.Sin(a2),
+            (a1, a2) => a1 * Math.Cos(a2),
+            (a1, a2) => a1 * Math.Tan(a2),
+            (a1, a2) => a1 * (1 / Math.Tan(a2))
+        };
+
+        public static double ConvertString(string expression) {
+            try {
+                var tokens = GetTokens(expression);
+            
+                var operandStack  = new Stack<double>();
+                var operatorStack = new Stack<string>();
+                var tokenIndex    = 0;
+
+                while (tokenIndex < tokens.Count) {
+                    var token = tokens[tokenIndex];
+                    switch (token) {
+                        case "(": {
+                            var subExpr = GetSubExpression(tokens, ref tokenIndex);
+                            operandStack.Push(ConvertString(subExpr));
+                            continue;
+                        }
+                        case ")":
+                            throw new ArgumentException("Неправильная скобочная последовательность!");
+                    }
+
+                    if (Array.IndexOf(Operators, token) >= 0) {
+                        while (operatorStack.Count > 0 && Array.IndexOf(Operators, token) < Array.IndexOf(Operators, operatorStack.Peek())) {
+                            var op    = operatorStack.Pop();
+                            var arg2 = operandStack.Pop();
+                            var arg1 = operandStack.Pop();
+                            operandStack.Push(Operations[Array.IndexOf(Operators, op)](arg1, arg2));
+                        }
+                        operatorStack.Push(token);
+                    }
+                    else operandStack.Push(double.Parse(token));
+                    
+                
+                    tokenIndex += 1;
+                }
+
+                while (operatorStack.Count > 0) {
+                    var op    = operatorStack.Pop();
+                    var arg2 = operandStack.Pop();
+                    var arg1 = operandStack.Pop();
+                
+                    operandStack.Push(Operations[Array.IndexOf(Operators, op)](arg1, arg2));
+                }
+                return operandStack.Pop();
+            }
+            catch (Exception e) {
+                MessageBox.Show($"{e}");
+                throw;
+            }
+        }
+
+        private static string GetSubExpression(IReadOnlyList<string> tokens, ref int index) {
+            var subExpr      = new StringBuilder();
+            var parentLevels = 1;
+            
+            index++;
+            while (index < tokens.Count && parentLevels > 0) {
+                var token = tokens[index];
+                if (tokens[index] == "(") parentLevels += 1;
+                
+                if (tokens[index++] == ")") parentLevels -= 1;
+                
+                if (parentLevels > 0) subExpr.Append(token + " ");
+            }
+            
+            if (parentLevels > 0) 
+                throw new ArgumentException("Неправильная скобочная последовательность!");
+            
+            return subExpr.ToString();
+        }
+
+        private static List<string> GetTokens(string expression) => expression.Split(" ", 
+            StringSplitOptions.RemoveEmptyEntries).ToList();
+    }
+}
---
+++ GraphDrawer/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using ExpressionConverter = GraphDrawer.Functions.ExpressionConverter;
+
+namespace GraphDrawer {
+    public partial class MainWindow {
+        public MainWindow() {
+            InitializeComponent();
+        }
+        
+        private string _equation = " ";
+        
+        private void UserPressButton(object sender, RoutedEventArgs e) {
+            var symbol = (sender as Button)!.Content.ToString();
+            _equation += (symbol == "ПРОБЕЛ" ? " " : symbol);
+            UserEquation.Content = _equation;
+        }
+
+        private void DeleteSymbol(object sender, RoutedEventArgs e) {
+            if (_equation.Length == 0) return;
+            
+            _equation = _equation[..^1];
+            UserEquation.Content = _equation;
+        }
+
+        [SuppressMessage("ReSharper.DPA", "DPA0000: DPA issues")]
+        private void ResolveEquation(object sender, RoutedEventArgs e) {
+            try {
+                GraphBody.Children.Clear();
+            
+                var prevX = .1d;
+                var prevY = ExpressionConverter.ConvertString(_equation.Replace("x", $"{prevX}"));
+                for (var i = .1d; i < 15; i += .1d) {
+                    var answer = ExpressionConverter.ConvertString(_equation.Replace("x", $"{i}"));
+
+                    GraphBody.Children.Add(new Line {
+                        X1 = prevX * 10,
+                        Y1 = -prevY * 10,
+                        X2 = i * 10,
+                        Y2 = -answer * 10,
+                        Stroke = Brushes.Black
+                    });
+                
+                    prevX = i;
+                    prevY = answer;
+                }
+            }
+            catch (Exception exception) {
+                MessageBox.Show($"{exception}");
+                throw;
+            }
+        }
+
+        private void ExportImage(object sender, RoutedEventArgs e) {
+            try {
+                var bounds = VisualTreeHelper.GetDescendantBounds(Graph);
+                const double dpi = 96d;
+                var rtb = new RenderTargetBitmap((int)bounds.Width, (int)bounds.Height, dpi, dpi, PixelFormats.Default);
+
+                var dv = new DrawingVisual();
+                using (var dc = dv.RenderOpen()) {
+                    var vb = new VisualBrush(Graph);
+                    dc.DrawRectangle(vb, null, new Rect(new Point(), bounds.Size));
+                }
+
+                rtb.Render(dv);
+
+                var file = new SaveFileDialog();
+                if (file.ShowDialog() != true) return;
+                
+                var png = new PngBitmapEncoder();
+                png.Frames.Add(BitmapFrame.Create(rtb));
+                using (Stream stm = File.Create(file.FileName)) {
+                    png.Save(stm);
+                }
+            }
+            catch (Exception exception) {
+                MessageBox.Show($"{exception}");
+                throw;
+            }
+        }
+    }
+}
